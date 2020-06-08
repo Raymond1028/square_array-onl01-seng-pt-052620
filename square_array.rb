f331dcf64bc85a8ed266f82012bc960b8b203251
Raymond Sims <lancelot53.rs@gmail.com>
2020-06-08 18:50:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,14 @@
 def square_array(array)
-  
+  new_array =[]
+    array .each do |num|
+        #x = num*num
+     new_array << num**2
+        
+    end
+      return new_array
+end
+puts square_array(numbers).inspect
+
+    
   # your code here
 end
